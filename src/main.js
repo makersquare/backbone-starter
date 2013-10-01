1e6f41d6a87bbd87f7ef8581d6335c602ca89e65
leejaew <lee.jaewang@gmail.com>
2013-10-01 16:04:41 -0500

    backbone.js exercise #2

--- src/main.js
+++ src/main.js
@@ -10,3 +10,24 @@ btn = new ButtonView({
 });
 
 btn.render();
+
+
+//Ex 2
+var ProfileView = Backbone.View.extend({
+	initialize: function(options){
+		this.name = options.name;
+		this.age = options.age;
+	},
+	render: function(){
+		$(this.el).html(this.name + "," + this.age)
+	}
+});
+
+var profileView = new ProfileView({
+	name: 'Bob',
+	age: 45,
+	el: $('.profile')
+})
+
+profileView.render();
+
