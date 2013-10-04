4cd5f6151366b503cfd130a6e403d29a089bde49
leejaew <lee.jaewang@gmail.com>
2013-10-04 09:52:09 -0500

    set the new price value on the model

--- src/models/stock.js
+++ src/models/stock.js
@@ -5,12 +5,21 @@
 
 (function () {
 
+	//Backbone Model
+	/* 
+	Backbone.Model.prototype.setByName = function(key, value, options) { 
+	    var setter = {}; 
+	    setter[key] = value; 
+	    this.set(setter, options); 
+	};
+	*/
   window.Stock = Backbone.Model.extend({
     updatePrice: function (newPrice) {
-
-      this.newPrice = parseFloat(newPrice)
-      console.log('Updating', this.get('name'), 'price to:', this.newPrice);
+	    this.newPrice = parseFloat(newPrice);
+	    this.set('newPrice', this.newPrice);
+			console.log('Updating', this.get('name'), 'price to:', this.newPrice);
     }
   });
+
 
 })();
