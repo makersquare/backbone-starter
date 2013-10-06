0662b71395cb2d658b927859fee5f901734cd668
Jesse Kevon <jessekevon@Jesses-MacBook-Pro.local>
2013-10-06 13:32:23 -0500

    listen for StockView model change in initialize

--- src/views/stock-view.js
+++ src/views/stock-view.js
@@ -1,8 +1,11 @@
+ // Listen for StockView's model's changes in initialize.
+
 (function () {
 
   window.StockView = Backbone.View.extend({
-    className: 'stock'
-    // TODO
-  });
-
-})();
+    className: 'stock',
+    initialize: function(){
+    	this.listenTo(this.model, 'change:price', this.render);
+    },
+	});
+});
